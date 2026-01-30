0c7184581674b8940108016ab8fd9a866a5faf6c
Elo <EloOfTheWired@proton.me>
2026-01-30 03:03:57 -0700

    improved the cmus module, and made the i3 module not be clickable

--- dotfiles/rei-theme/.config/polybar/player-cmus.sh
+++ dotfiles/rei-theme/.config/polybar/player-cmus.sh
@@ -1,48 +1,63 @@
-#!/bin/bash
+#!/bin/sh
 
-width=35
-sleep_time=0.2
-padding="   "   # space between loops
+info_string="cmus not running"
 
-last_info=""
+updateInfo () {
+
+	if info=$(cmus-remote -Q 2> /dev/null); then
+		status=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "status " | cut -d ' ' -f 2)
+
+		if [ "$status" = "playing" ] || [ "$status" = "paused" ] || [ "$status" = "stopped" ]; then
+			title=$(echo "$info" | grep -v 'set ' | grep " title " | cut -d ' ' -f 3-)
+			artist=$(echo "$info" | grep -v 'set ' | grep " artist " | cut -d ' ' -f 3-)
+			position=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "position " | cut -d ' ' -f 2)
+			duration=$(echo "$info" | grep -v "set " | grep -v "tag " | grep "duration " | cut -d ' ' -f 2)
+
+			if [ "$artist" ] || [ "$title" ]; then
+
+				info_string="$artist - $title"
+
+				if [ "$status" = "playing" ]; then
+					info_string="(playing) $info_string"
+				elif [ "$status" = "paused" ]; then
+					info_string="(paused) $info_string"
+				elif [ "$status" = "stopped" ]; then
+					info_string="(stopped) $info_string"
+				else
+					info_string="cmus error"
+				fi
+			else
+				info_string="no title or artist for this song"
+			fi
+		else
+			info_string="no title or artist for this song"
+		fi
+	else
+		info_string="cmus not running"
+	fi
+
+
+}
 
 while true; do
-    info=$(cmus-remote -Q 2>/dev/null)
 
-    if [ $? -ne 0 ]; then
-        echo "[cmus not running]"
-        sleep 2
-        continue
-    fi
+	updateInfo
+	text="$info_string"
+	info_string="$info_string $info_string"
+	end=${#text}
+	text="$info_string"
 
-    status=$(echo "$info" | grep "^status " | cut -d' ' -f2)
-    artist=$(echo "$info" | grep "^tag artist " | cut -d' ' -f3-)
-    title=$(echo "$info"  | grep "^tag title "  | cut -d' ' -f3-)
-
-    if [ -z "$artist" ] && [ -z "$title" ]; then
-        text="[nothing playing]"
-    else
-        text="($status) $artist - $title"
-    fi
-
-    # Only rebuild scroll if song changed
-    if [ "$text" != "$last_info" ]; then
-        last_info="$text"
-
-        # Build seamless loop
-        scroll_text="$text$padding$text$padding"
-
-        len=${#scroll_text}
-        pos=0
-    fi
-
-    # Output one frame
-    printf "%s\n" "${scroll_text:pos:width}"
-
-    # Advance position
-    pos=$(( (pos + 1) % len ))
-
-    sleep "$sleep_time"
+	for (( i = 0; i < "${#text}"; i++ )); do
+		if [[ $i -eq ${end} ]]; then
+			i=0
+		fi
+		echo "${text:$i:${end}}"
+		sleep 0.4
+		updateInfo
+		info_string="$info_string $info_string"
+		if [[ $info_string != $text ]]; then
+			break
+		fi
+	done
 done
-
 
