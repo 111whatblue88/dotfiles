7a5ca503a7456c1627c22ad12dc0024a32461a86
Elo <EloOfTheWired@proton.me>
2026-03-12 20:52:44 -0700

    added multiple monitor support for polybar

--- dotfiles/rei-theme/.config/polybar/start.sh
+++ dotfiles/rei-theme/.config/polybar/start.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-polybar i3bar &
-polybar storagebar &
-polybar hardwarebar &
+polybarInitPrimary () {
 
-polybar cmusbar &
-polybar softwarebar & 
+  MONITOR=$1 polybar cmusbar &
+  MONITOR=$1 polybar i3bar &
+  MONITOR=$1 polybar storagebar &
+  MONITOR=$1 polybar hardwarebar &
+  MONITOR=$1 polybar softwarebar &
+  sleep 2
+  MONITOR=$1 polybar textbar &
+}
+polybarInitMinimal () {
+  polybar textbar $1 &
+}
 
-sleep 0.5
-polybar textbar &
+numMonitors=$(polybar -m | wc -l | awk '{print $1}')
+monitors=($(xrandr | grep -w connected | awk '{print $1}'))
+
+if [[ $numMonitors = "1" ]]; then 
+  polybarInitPrimary ${monitors[0]}
+  exit
+fi
+
+primaryMonitor=$(xrandr | grep -w primary | awk '{print $1}')
+
+for monitor in ${monitors[@]}; do
+
+  if [[ $monitor = primaryMonitor ]]; then 
+    continue 
+  fi
+
+  polybarInitMinimal $monitor
+
+done
+
+
